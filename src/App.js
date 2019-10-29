445a87559f3ddbdd5e4a215c5d0a3675f96b2a1a
Daniel A Marcano Torres <55719451+danmarct@users.noreply.github.com>
2019-10-29 09:47:09 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return ( < div className = "App" >
+
+    <style>
+
+    </style>
+
+<body>
+      <h1>Tips and tutorials</h1>
+
+      <h2>How to install React.</h2>
+      <ol>
+      <li>this is a step</li>
+      <div>
+        <pre>
+          <code>
+          <p>this is code</p>
+          </code>
+        </pre>
+      </div>
+
+      <li>this is another step</li>
+      </ol>
+
+    </div>
+  </body>
+);
+}
+
+export default App;
